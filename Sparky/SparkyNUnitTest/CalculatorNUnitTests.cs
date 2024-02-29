2be4a3e05ffe54f6d69e5063ef7338f28096b4d2
parasrawat007 <parasrawat67@gmail.com>
2024-02-29 08:14:35 +0530

    Testing multiple values

--- Sparky/SparkyNUnitTest/CalculatorNUnitTests.cs
+++ Sparky/SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -15,25 +15,27 @@ namespace SparkyNUnitTest
 
             //Act
             int result = calc.AddNumbers(10, 20);
-           
+
             //Assert
             Assert.AreEqual(30, result);
-           
-           
+
+
         }
         [Test]
-        public void IsOddChecker_InputEvenNumber_ReturnFalse() 
-        { 
+        public void IsOddChecker_InputEvenNumber_ReturnFalse()
+        {
             Calculator calc = new Calculator();
             bool isOdd = calc.IsOddNumber(2);
-            Assert.That(isOdd,Is.EqualTo(false));
+            Assert.That(isOdd, Is.EqualTo(false));
         }
 
         [Test]
-        public void IsOddChecker_InputOddNumber_ReturnTrue()
+        [TestCase(11)]
+        [TestCase(13)]
+        public void IsOddChecker_InputOddNumber_ReturnTrue(int a)
         {
             Calculator calc = new Calculator();
-            bool isOdd = calc.IsOddNumber(3);
+            bool isOdd = calc.IsOddNumber(a);
             Assert.IsTrue(isOdd);
         }
     }
