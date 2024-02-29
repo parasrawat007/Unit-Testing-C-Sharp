e81fdad4c4ba389fbf401afce5699cbba1b42670
parasrawat007 <parasrawat67@gmail.com>
2024-02-29 08:34:31 +0530

    Asserting String Values

---
+++ Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class CustomerNUnitTests
+    {
+        [Test]
+        public void CombineName_InputFirstAndLastName_ReturnFullName()
+        {
+            //Arrange
+            var customer = new Customer();
+            //Act
+            var fullName=customer.CombineNames("Ben", "Spark");
+            //Assert
+            Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
+        }
+
+    }
+}
