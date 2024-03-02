27464284bef51df2d81ce1f237da0382c5f731d9
parasrawat007 <parasrawat67@gmail.com>
2024-03-02 12:12:10 +0530

    String Assert Helper Methods

--- Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
+++ Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SparkyNUnitTest
+namespace SparkyNUnitTest 
 {
     [TestFixture]
     public class CustomerNUnitTests
@@ -16,10 +16,24 @@ namespace SparkyNUnitTest
         {
             //Arrange
             var customer = new Customer();
+            
             //Act
             var fullName=customer.CombineNames("Ben", "Spark");
+
             //Assert
+            Assert.AreEqual(fullName, "Hello, Ben Spark");
             Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
+
+            Assert.That(fullName, Does.Contain(","));
+            Assert.That(fullName, Does.Contain("Ben Spark"));
+            Assert.That(fullName, Does.Contain("ben Spark").IgnoreCase);
+
+            Assert.That(fullName, Does.StartWith("Hello,"));
+            Assert.That(fullName, Does.EndWith("Spark"));
+
+            //Macthiing with regular expression
+            Assert.That(fullName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
+
         }
 
     }
