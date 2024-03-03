78285c717ba48febeac05889eba0b9badd10dad7
parasrawat007 <parasrawat67@gmail.com>
2024-03-03 09:20:40 +0530

    Setup Bank Account Class, Setup Logger Class

---
+++ Sparky/Sparky/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public class BankAccount
+    {
+        private readonly ILogBook _logBook;
+        public int Balance { get; set; }
+
+        public BankAccount(ILogBook logBook)
+        {
+            Balance = 0;
+            _logBook=logBook;
+        }
+
+        public bool Deposit(int amount)
+        {
+            _logBook.Message("Deposit Invoked");
+            Balance += amount;
+            return true;
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                return true;
+            }
+            return false;
+            
+        }
+
+        public int GetBalance() 
+        { 
+            return Balance;
+        }
+    }
+}
---
+++ Sparky/SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class BankAccountNUnitTests
+    {
+        private BankAccount bankAccount;
+        
+        [SetUp]
+        public void Setup()
+        {
+            bankAccount = new(new LogBook());
+        }
+
+        [Test]
+        public void BankDeposit_Add100_ReturnsTrue() 
+        {
+            var result = bankAccount.Deposit(100);
+            Assert.IsTrue(result);
+            Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
+        }
+    }
+}
