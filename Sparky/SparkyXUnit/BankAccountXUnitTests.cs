a9140dffd3e5adc602afe42b2a5813615e5f590e
parasrawat007 <parasrawat67@gmail.com>
2024-03-05 19:24:38 +0530

    Create Xunit Project

---
+++ Sparky/SparkyXUnit/BankAccountXUnitTests.cs
@@ -0,0 +1,154 @@
+﻿using Moq;
+using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class BankAccountXUnitTests
+    {
+        private BankAccount account;
+        
+        [SetUp]
+        public void Setup()
+        {
+          
+        } 
+
+        //[Test]
+        //public void BankDepositlogFakker_Add100_ReturnsTrue() 
+        //{
+        //    BankAccount bankAccount = new(new LogFakker());
+        //    var result = bankAccount.Deposit(100);
+        //    Assert.IsTrue(result);
+        //    Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
+        //}
+
+        [Test]
+        public void BankDeposit_Add100_ReturnsTrue()
+        {
+            var logMock = new Mock<ILogBook>();
+            BankAccount bankAccount = new(logMock.Object);
+
+            var result = bankAccount.Deposit(100);
+
+            Assert.IsTrue(result);
+            Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
+        }
+        
+        [Test]
+        [TestCase(200,100)]
+        [TestCase(200,150)]
+        public void BankWitdrawn_Withdraw100With200Balance_ReturnsTrue(int balance, int withdraw)
+        { 
+            var logMock = new Mock<ILogBook>();
+            logMock.Setup(x => x.LogToDb(It.IsAny<String>())).Returns(true);
+            logMock.Setup(x=>x.LogBalanceAfterWithdrawl(It.Is<int>(i=>i>0))).Returns(true);
+
+            BankAccount account = new(logMock.Object);
+            account.Deposit(balance);  
+            var result= account.Withdraw(withdraw);
+            Assert.IsTrue(result);  
+
+        }
+
+        [Test]
+        [TestCase(200, 300)]
+        public void BankWitdrawn_Withdraw300With200Balance_ReturnsTrue(int balance, int withdraw)
+        {
+            var logMock = new Mock<ILogBook>();
+            //logMock.Setup(x => x.LogBalanceAfterWithdrawl(It.Is<int>(i => i < 0))).Returns(false);
+            logMock.Setup(x => x.LogBalanceAfterWithdrawl(It.IsInRange<int>(int.MinValue,-1,Moq.Range.Inclusive))).Returns(false);
+            BankAccount account = new(logMock.Object);
+            account.Deposit(balance);
+            var result = account.Withdraw(withdraw);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void BankLogDummy_LogMockString_ReturnTrue()
+        {
+            var logMock = new Mock<ILogBook>();
+            string desiredOutput = "Hello";
+            logMock.Setup(u => u.MessageWithReturnStr("Hi")).Returns((string str) => str.ToLower());
+            Assert.That(logMock.Object.MessageWithReturnStr("HELLO"),Is.EqualTo(desiredOutput.ToLower()));
+        }
+        [Test]
+        public void BankLogDummy_LogMockStringOutputStr_ReturnTrue()
+        {
+            var logMock = new Mock<ILogBook>();
+            string desiredOutput = "Hello";
+            logMock.Setup(u => u.LogWithOutputResult(It.IsAny<String>(),out desiredOutput)).Returns(true);
+            var result = "";
+            Assert.IsTrue(logMock.Object.LogWithOutputResult("Ben",out result));
+            Assert.That(result, Is.EqualTo(desiredOutput));
+        }
+        [Test]
+        public void BankLogDummy_LogRefChecker_ReturnTrue()
+        {
+            var logMock = new Mock<ILogBook>();
+
+            Customer customer = new Customer();
+            Customer customerNotUsed = new Customer();
+
+            logMock.Setup(u => u.LogWithRefObj(ref customer)).Returns(true);
+
+            Assert.IsTrue(logMock.Object.LogWithRefObj(ref customer));
+            Assert.IsFalse(logMock.Object.LogWithRefObj(ref customerNotUsed));
+        }
+        [Test]
+        public void BankLogDummy_LogRefCheckerSetAndGetLogSeverityMock_MockTest()
+        {
+            var logMock = new Mock<ILogBook>();
+
+            logMock.SetupAllProperties(); //now it will work because order matters
+
+            logMock.Setup(u => u.LogSeverity).Returns(10);
+            logMock.Setup(u => u.LogType).Returns("Warning");
+            //logMock.Object.LogSeverity = 100; Doesnot work invalid but wouldnot give error
+
+            //logMock.SetupAllProperties(); This way it will work
+            //logMock.Object.LogSeverity = 100; will also give error for warning
+            Assert.That(logMock.Object.LogSeverity, Is.EqualTo(10));
+            Assert.That(logMock.Object.LogType, Is.EqualTo("Warning"));
+
+            //callbacks
+            string logTemp = "Hello, ";
+            logMock.Setup(u => u.LogToDb(It.IsAny<string>()))
+                .Returns(true)
+                .Callback((string str) => logTemp += str);
+            logMock.Object.LogToDb("Ben");
+            Assert.That(logTemp,Is.EqualTo("Hello, Ben"));
+
+            //callbacks
+            int counter = 5;
+            logMock.Setup(u => u.LogToDb(It.IsAny<string>()))
+                .Callback((string str) => counter++)
+                .Returns(true)
+                .Callback((string str) => counter++);
+            logMock.Object.LogToDb("Ben");
+            Assert.That(counter, Is.EqualTo(7));
+        }
+        [Test]
+        public void BankLogDummy_VerifyExample()
+        {
+            var logMock = new Mock<ILogBook>();
+            var bankAccount = new BankAccount(logMock.Object);
+            bankAccount.Deposit(100);
+            Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
+
+            //verification
+            logMock.Verify(u => u.Message(It.IsAny<string>()), Times.Exactly(2));
+            logMock.Verify(u => u.Message( "Deposit Invoked"), Times.AtLeastOnce);
+            logMock.Verify(u => u.Message("Test"), Times.AtLeastOnce);
+            logMock.VerifySet(u => u.LogSeverity = 101, Times.Once);
+            logMock.VerifyGet(u => u.LogSeverity , Times.Once);
+
+        }
+    }
+}
---
+++ Sparky/SparkyXUnit/CalculatorXUnitTests.cs
@@ -0,0 +1,91 @@
+﻿
+using NUnit.Framework;
+using Sparky;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class CalculatorXUnitTests
+    {
+        private Calculator calc;
+
+        [SetUp]
+        public void SetUp() 
+        {
+            //Arrange --> Initialization
+            calc = new Calculator();
+        }
+
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Act
+            int result = calc.AddNumbers(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+
+        }
+
+        [Test]
+        [TestCase(5.4,10.5)] //15.9
+        [TestCase(5.43,10.53)]//15.93
+        [TestCase(5.49,10.59)]//16.08
+        public void AddNumbersDouble_InputTwoDouble_GetCorrectAddition(double a,double b)
+        {
+            //Arrange --> Initialization
+            Calculator calc = new Calculator();
+
+            //Act
+            double result = calc.AddNumbersDouble(a, b);
+
+            //Assert
+            Assert.AreEqual(15.9, result,.2);
+        }
+
+        [Test]
+        public void IsOddChecker_InputEvenNumber_ReturnFalse()
+        {
+            bool isOdd = calc.IsOddNumber(2);
+            Assert.That(isOdd, Is.EqualTo(false));
+        }
+
+        [Test]
+        [TestCase(11)]
+        [TestCase(13)]
+        public void IsOddChecker_InputOddNumber_ReturnTrue(int a)
+        {
+            bool isOdd = calc.IsOddNumber(a);
+            Assert.IsTrue(isOdd);
+        }
+
+        [Test]
+        [TestCase(10, ExpectedResult = false)]
+        [TestCase(11, ExpectedResult = true)]
+        public bool IsOddChecker_Number_ReturnTrueIfOdd(int a)
+        { 
+            Calculator calculator = new Calculator();
+            return calculator.IsOddNumber(a);   
+        }
+
+        [Test]
+        public void OddRanger_InputMinAndMaxRange_ReturnsValidOddNumberRange()
+        {
+            List<int> expectedOddRange = new() { 5, 7, 9 };
+            
+            //Act
+            var result = calc.GetOddRange(5,10);
+
+            //Assert
+            Assert.That(result, Is.EquivalentTo(expectedOddRange));
+            Assert.AreEqual(expectedOddRange, result);
+            Assert.Contains(7, result);
+            Assert.That(result,Does.Contain(7));
+            Assert.That(result,Is.Not.Empty);
+            Assert.That(result,Has.No.Member(6));
+            Assert.That(result,Is.Ordered);
+            Assert.That(result,Is.Unique);
+        }
+    }
+
+}
