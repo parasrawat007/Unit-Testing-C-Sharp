f7efeb3942ac6c6404937d9c194d7515e64a7b32
parasrawat007 <parasrawat67@gmail.com>
2024-03-05 22:33:02 +0530

    MOQ and Xunit

--- Sparky/SparkyXUnit/ProductXUnitTests.cs
+++ Sparky/SparkyXUnit/ProductXUnitTests.cs
@@ -1,38 +1,32 @@
-﻿//using Moq;
-//using NUnit.Framework;
-//using Sparky;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Moq;
+using Sparky;
+using Xunit;
 
-//namespace SparkyNUnitTest
-//{
-//    [TestFixture]
-//    public class ProductXUnitTests
-//    {
-//        [Test]
-//        public void GetProductPrice_PlatinumCustomer_ReturnPriceWith20Discount()
-//        { 
-//            Product product = new Product() { Price = 50 };
-          
-//            var result = product.GetPrice(new Customer() { IsPlatinum = true });
-           
-//            Assert.That(result, Is.EqualTo(40));
-//        }
+namespace SparkyNUnitTest
+{
+    public class ProductXUnitTests
+    {
+        [Fact]
+        public void GetProductPrice_PlatinumCustomer_ReturnPriceWith20Discount()
+        {
+            Product product = new Product() { Price = 50 };
 
-//        [Test]
-//        public void GetProductPriceMOQAbuse_PlatinumCustomer_ReturnPriceWith20Discount()
-//        {
-//            var customer = new Mock<ICustomer>();
-//            customer.Setup(u=>u.IsPlatinum).Returns(true);
+            var result = product.GetPrice(new Customer() { IsPlatinum = true });
 
-//            Product product = new Product() { Price = 50 };
+            Assert.Equal(40,result);
+        }
 
-//            var result = product.GetPrice(customer.Object);
+        [Fact]
+        public void GetProductPriceMOQAbuse_PlatinumCustomer_ReturnPriceWith20Discount()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(u => u.IsPlatinum).Returns(true);
 
-//            Assert.That(result, Is.EqualTo(40));
-//        }
-//    }
-//}
+            Product product = new Product() { Price = 50 };
+
+            var result = product.GetPrice(customer.Object);
+
+            Assert.Equal(40, result);
+        }
+    }
+}
