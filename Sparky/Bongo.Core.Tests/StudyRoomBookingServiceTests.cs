f8413bae99e0d8bd800e0c42411d87a7addc93c7
parasrawat007 <parasrawat67@gmail.com>
2024-03-11 19:31:37 +0530

    Bongo Core Save Booking Setup

--- Sparky/Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
+++ Sparky/Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -1,19 +1,39 @@
 using Bongo.Core.Services;
 using Bongo.DataAccess.Repository.IRepository;
+using Bongo.Models.Model;
 using Moq;
 
 namespace Bongo.Core.Tests
 {
     public class StudyRoomBookingServiceTests
     {
+        private StudyRoomBooking _request;
+        private IList<StudyRoom> _availableStudyRoom;
         private Mock<IStudyRoomBookingRepository> _studyRoomBookingRepositoryMock;
         private Mock<IStudyRoomRepository> _studyRoomRepositoryMock;
         private StudyRoomBookingService _studyRoomBookingService;
 
         public StudyRoomBookingServiceTests()
         {
+            _request = new()
+            {
+                BookingId=1,
+                Date= DateTime.Now.AddDays(1),
+                Email="ben@gmail.com",
+                
+            };
+            _availableStudyRoom= new List<StudyRoom>()
+            { 
+                new StudyRoom() 
+                {
+                    Id=1,
+                    RoomName="Michigan",
+                    RoomNumber="A202"
+                }
+            };
             _studyRoomBookingRepositoryMock = new Mock<IStudyRoomBookingRepository>();
             _studyRoomRepositoryMock = new Mock<IStudyRoomRepository>();
+            _studyRoomRepositoryMock.Setup(x => x.GetAll()).Returns(_availableStudyRoom);
             _studyRoomBookingService = new StudyRoomBookingService(
                     _studyRoomBookingRepositoryMock.Object,
                     _studyRoomRepositoryMock.Object
@@ -34,5 +54,6 @@ namespace Bongo.Core.Tests
             Assert.Equal("Value cannot be null. (Parameter 'request')", exception.Message);
             Assert.Equal("request", exception.ParamName);
         }
+       
     }
 }
